5d4765281eaca94c851ff886af299792ff30d951
Bob Tiernay <btiernay@oicr.on.ca>
2015-12-09 08:08:36 -0500

    Deployed ca555a5 with MkDocs version: 0.14.0

--- js/base.js
+++ js/base.js
@@ -20,15 +20,18 @@ $(function () {
    var $body = $("#body");
 
    // Add header links
-   $(":header", $body).each(function(i, header) {
-    var $header = $(header);
-    var id = $header.attr('id');
-    var icon = '<i class="fa fa-link"></i>';
+   $(":header", $body).each(function (i, header) {
+      var $header = $(header);
+      var id = $header.attr('id');
+      var icon = '<i class="fa fa-link"></i>';
 
-    if (id) {
-      return $header.prepend($("<a/>").addClass("header-link").attr("href", "#" + id).html(icon));
-    }
-  });
+      if (id) {
+         var title = $header.text();
+         $header.text("");
+         $header.prepend($("<a/>").addClass("header-link").attr("href", "#" + id).html(icon));
+         $header.append($("<a/>").addClass("header-text-link").attr("href", "#" + id).text(title));
+      }
+   });
 
    $.get(base_url + '/mkdocs/search_index.json', function (data) {
       var index = lunr(function () {
@@ -62,7 +65,7 @@ $(function () {
             return;
          }
 
-         $results.delegate("a", "click", function(){
+         $results.delegate("a", "click", function () {
             $body.show();
             $results.hide();
          });
