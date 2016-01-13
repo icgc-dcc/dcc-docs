f5d79568b7964f5b39b64ae11f724bee4855c721
Bob Tiernay <btiernay@oicr.on.ca>
2016-01-13 12:12:38 -0500

    Deployed 25fcae3 with MkDocs version: 0.15.0.dev

--- js/icgc-common.js
+++ js/icgc-common.js
@@ -7,6 +7,195 @@ $(function() {
       window.$icgcApp = {config: {}};
     }
 
+
+    function _initSearch(confineToContainerID) {
+
+      //
+      function _debounce(func, wait, immediate) {
+        var _timeout;
+
+        return function () {
+
+          var context = this,
+            args = arguments,
+            later = function () {
+              _timeout = null;
+              if (!immediate) func.apply(context, args);
+            };
+
+          var callNow = immediate && !_timeout;
+
+          clearTimeout(_timeout);
+
+          _timeout = setTimeout(later, wait);
+
+          if (callNow) {
+            func.apply(context, args);
+          }
+        };
+      }
+      ////////////////////////////////////////////
+
+      //
+      function _addSearchListeners() {
+
+        var $search = $('.searchbox'),
+            submitIcon = $('.searchbox-icon'),
+            inputBox = $('.searchbox-input'),
+            isOpen = false;
+
+          $search.submit(function (e) {
+          e.preventDefault();
+          submitIcon.click();
+        });
+
+        submitIcon.click(function () {
+          if (isOpen == false) {
+            inputBox.val('');
+            $search.addClass('searchbox-open');
+            inputBox.focus();
+            isOpen = true;
+          }
+          else {
+            $search.removeClass('searchbox-open');
+            inputBox.focusout();
+            isOpen = false;
+          }
+        });
+
+        submitIcon.mouseup(function () {
+          return false;
+        });
+
+        $search.mouseup(function () {
+          return false;
+        });
+
+        $(document).mouseup(function () {
+          if (isOpen == true) {
+            $('.searchbox-icon').css('display', 'block');
+            submitIcon.click();
+          }
+        });
+      }
+      ////////////////////////////////////////////
+
+      //
+      function _getSearchTerm() {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++) {
+          var sParameterName = sURLVariables[i].split('=');
+          if (sParameterName[0] == 'q') {
+            return decodeURIComponent(sParameterName[1].replace(/\+/g, '%20'));
+          }
+        }
+      }
+      ////////////////////////////////////////////
+
+      function _initSearchIndex() {
+        var $query = $('.searchbox-input'),
+            $body = $(confineToContainerID),
+            $resultsContainer = $('#mkdocs-search-results-container'),
+            $results = $('#mkdocs-search-results'),
+            $searchContentBody = $('#search-body');
+
+        $.get(base_url + '/mkdocs/search_index.json', function (data) {
+          var index = lunr(function () {
+            this.field('title', {
+              boost: 10
+            });
+            this.field('text');
+            this.ref('location');
+          });
+
+          var documents = {},
+              doc;
+
+          for (var i = 0; i < data.docs.length; i++) {
+            doc = data.docs[i];
+            doc.location = base_url + doc.location;
+            index.add(doc);
+            documents[doc.location] = doc;
+          }
+
+          function __search() {
+            var query = $query.val();
+
+
+            $results.empty();
+
+            if (query.length < 3 || query === '') {
+              $body.show();
+              $resultsContainer.hide();
+
+              return;
+            }
+
+            $results.delegate("a", "click", function () {
+              $body.show();
+              $resultsContainer.hide();
+            });
+
+            var results = index.search(query),
+                resultsHTML = '';
+
+            if (results.length > 0) {
+              $body.hide();
+              $resultsContainer.show();
+              $searchContentBody.html('<strong>' + results.length  + '</strong> results found for <strong>' + query  + '</strong>' );
+
+              for (var i = 0; i < results.length; i++) {
+                var result = results[i];
+                doc = documents[result.ref];
+                doc.base_url = base_url;
+                doc.summary = doc.text.substring(0, 200);
+
+                resultsHTML += '<div class="search-item animated fadeInLeft">' +
+                               '<div class="doc-type-icon-container"><span class="header-badge"><i class="icon-book-open"></i></span></div>' +
+                               '<div class="search-body">' +
+                               '<a href="' + doc.location + '">' + doc.title + '</a>' +
+                               '<p>' + doc.summary + '</p>' +
+                               '</div>' +
+
+                               '</div>';
+                console.log(query);
+
+              }
+
+              $results.append(resultsHTML);
+              $results.highlight(query);
+            }
+            else {
+              $body.show();
+            }
+          }
+
+          var searchInput = document.getElementById('mkdocs-search-query');
+
+          var term = _getSearchTerm();
+
+          if (term) {
+            searchInput.value = term;
+            search();
+          }
+
+          searchInput.addEventListener('keyup', _debounce(__search, 300));
+        });
+
+      }
+
+
+
+      function _init() {
+        _addSearchListeners();
+        _initSearchIndex();
+      }
+
+      _init();
+
+    }
+
     function _initHeaders(confineToContainerID) {
       var header = $(confineToContainerID + ' h1');
       if (header.hasClass('no-auto-render')) {
@@ -113,6 +302,7 @@ $(function() {
     _initMenuNavBar('.navbar-nav', '> li');
     _initHeaders(BODY_ID);
     _initLinks(BODY_ID);
+    _initSearch(BODY_ID);
     _calcMainContentWidth();
   }
 
