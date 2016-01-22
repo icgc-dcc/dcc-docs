a0e009053fe6b8a9cf3de5e8b40645274a2a3451
Michael Moncada <michael.moncada@oicr.on.ca>
2016-01-22 16:50:52 -0500

    """

--- theme/js/icgc-common.js
+++ theme/js/icgc-common.js
@@ -310,7 +310,7 @@ $(function() {
 
         ///console.log(pageProgressPercentage - initPageProgressPercentage, offset);
         if (evalPercent > 2 && evalPercent < 98) {
-          resizingEl.scrollTop(offset - 50);
+          resizingEl.scrollTop(offset - 30);
         }
         else if  (evalPercent < 2) {
           resizingEl.scrollTop(0);
