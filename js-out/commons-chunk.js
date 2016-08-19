7d9255ff1eeda53dbceddf657f0cb0854dccf89e
btiernay <btiernay@oicr.on.ca>
2016-08-19 12:44:05 -0500

    Deployed a370995 with MkDocs version: 0.15.3

--- js-out/commons-chunk.js
+++ js-out/commons-chunk.js
@@ -30,7 +30,7 @@
 /******/ 	// "0" means "already loaded"
 /******/ 	// Array means "loading", array contains callbacks
 /******/ 	var installedChunks = {
-/******/ 		1:0
+/******/ 		2:0
 /******/ 	};
 
 /******/ 	// The require function
@@ -76,7 +76,7 @@
 /******/ 			script.charset = 'utf-8';
 /******/ 			script.async = true;
 
-/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + ({"0":"dictionary-viewer"}[chunkId]||chunkId) + ".js";
+/******/ 			script.src = __webpack_require__.p + "" + chunkId + "." + ({"0":"dictionary-viewer","1":"software-page"}[chunkId]||chunkId) + ".js";
 /******/ 			head.appendChild(script);
 /******/ 		}
 /******/ 	};
--- js-out/dictionary-viewer.js
+++ js-out/dictionary-viewer.js
@@ -1,61 +1,60 @@
-webpackJsonp([0,1],[
+webpackJsonp([0,2],[
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
+	'use strict';
+
 	__webpack_require__(1);
 
-	angular.module('DocsDictionaryViewerApp', ['DictionaryViewerApp', 'ngAnimate', 'chieffancypants.loadingBar'])
-	  .controller('DictionaryViewerCtrl', function ($scope, $timeout, $location, $anchorScroll, DictionaryService, DictionaryViewerConstants) {
-	    var _controller = this,
+	angular.module('DocsDictionaryViewerApp', ['DictionaryViewerApp', 'ngAnimate', 'chieffancypants.loadingBar']).controller('DictionaryViewerCtrl', function ($scope, $timeout, $location, $anchorScroll, DictionaryService, DictionaryViewerConstants) {
+	  var _controller = this,
 	      _firstRun = true;
 
-	    _controller.searchQuery = '';
-	    _controller.baseDictionaryURL = window.$icgcApp.dictionaryViewer.config.baseDictionaryURL || 'https://submissions.dcc.icgc.org';
+	  _controller.searchQuery = '';
+	  _controller.baseDictionaryURL = window.$icgcApp.dictionaryViewer.config.baseDictionaryURL || 'https://submissions.dcc.icgc.org';
 
-	    _controller.viewTypes = DictionaryService.getViewTypes();
+	  _controller.viewTypes = DictionaryService.getViewTypes();
 
-	    _controller.switchToReportView = function (reportAnchor) {
-	      var search = $location.search();
+	  _controller.switchToReportView = function (reportAnchor) {
+	    var search = $location.search();
 
-	      _controller.viewMode = 'report';
-	      search.viewMode = _controller.viewMode;
+	    _controller.viewMode = 'report';
+	    search.viewMode = _controller.viewMode;
 
-	      $location.search(search).hash(reportAnchor);
-	    };
+	    $location.search(search).hash(reportAnchor);
+	  };
 
-	    _controller.setDictionaryVersionFilterRange = DictionaryService.setDictionaryFilterRange;
-	    _controller.versionRange = DictionaryService.dictionaryVersionRange();
-	    _controller.getDictionaryVersionList = DictionaryService.getDictionaryVersionList;
-	    _controller.shouldCompareDictionaries = false;
+	  _controller.setDictionaryVersionFilterRange = DictionaryService.setDictionaryFilterRange;
+	  _controller.versionRange = DictionaryService.dictionaryVersionRange();
+	  _controller.getDictionaryVersionList = DictionaryService.getDictionaryVersionList;
+	  _controller.shouldCompareDictionaries = false;
 
-	    $scope.$on(DictionaryViewerConstants.EVENTS.RENDER_COMPLETE, function () {
-	      DictionaryService.generateChangeList().then(function (changeReport) {
+	  $scope.$on(DictionaryViewerConstants.EVENTS.RENDER_COMPLETE, function () {
+	    DictionaryService.generateChangeList().then(function (changeReport) {
 
-	        _controller.fieldsAddedCount = changeReport.fieldsAdded.length;
-	        _controller.fieldsChangedCount = changeReport.fieldsChanged.length;
-	        _controller.fieldsRemovedCount = changeReport.fieldsRemoved.length;
-	        _controller.latestDictionaryVersion = DictionaryService.getLatestDictionaryVersion();
+	      _controller.fieldsAddedCount = changeReport.fieldsAdded.length;
+	      _controller.fieldsChangedCount = changeReport.fieldsChanged.length;
+	      _controller.fieldsRemovedCount = changeReport.fieldsRemoved.length;
+	      _controller.latestDictionaryVersion = DictionaryService.getLatestDictionaryVersion();
 
-	        if (!_controller.shouldCompareDictionaries) {
-	          _controller.shouldCompareDictionaries = _controller.versionRange.from !== _controller.versionRange.to;
-	        }
+	      if (!_controller.shouldCompareDictionaries) {
+	        _controller.shouldCompareDictionaries = _controller.versionRange.from !== _controller.versionRange.to;
+	      }
 
-	        if (_firstRun) {
+	      if (_firstRun) {
 
-	          _firstRun = false;
+	        _firstRun = false;
 
-	          $timeout(function () {
-	            if ($location.hash()) {
-	              $anchorScroll.yOffset = 30;
-	              $anchorScroll();
-	            }
-	          }, 0);
-	        }
-
-	      });
+	        $timeout(function () {
+	          if ($location.hash()) {
+	            $anchorScroll.yOffset = 30;
+	            $anchorScroll();
+	          }
+	        }, 0);
+	      }
 	    });
 	  });
-
+	});
 
 /***/ },
 /* 1 */
