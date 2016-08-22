b7e9b50818f9c8ac45a4da85e971ee7b9aba183d
GavinMWilson <Gavin.M.Wilson@oicr.on.ca>
2016-08-22 14:42:05 -0500

    Deployed a925b23 with MkDocs version: 0.15.3

--- js-out/software-page.js
+++ js-out/software-page.js
@@ -5,7 +5,7 @@ webpackJsonp([1,2],{
 
 	/* WEBPACK VAR INJECTION */(function(global) {'use strict';
 
-	var _angular = __webpack_require__(68);
+	var _angular = __webpack_require__(69);
 
 	var _angular2 = _interopRequireDefault(_angular);
 
@@ -109,16 +109,16 @@ webpackJsonp([1,2],{
 
 /***/ },
 
-/***/ 68:
+/***/ 69:
 /***/ function(module, exports, __webpack_require__) {
 
-	__webpack_require__(69);
+	__webpack_require__(70);
 	module.exports = angular;
 
 
 /***/ },
 
-/***/ 69:
+/***/ 70:
 /***/ function(module, exports) {
 
 	/**
