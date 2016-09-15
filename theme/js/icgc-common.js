5971c1eae38a0b72fd10d7ce640eed3625fa554c
Chang Wang <garnwraly@gmail.com>
2016-09-15 12:41:54 -0400

    bugfix: DCC-5203 click twice on link scrolls to wrong position

--- theme/js/icgc-common.js
+++ theme/js/icgc-common.js
@@ -71,7 +71,7 @@ $(function() {
 
         // animate
         scrollBody.animate({
-          scrollTop: scrollTargetEl.offset().top + 7
+          scrollTop: scrollTargetEl.get(0).offsetTop + 7
         }, 300, function(){
 
           var targetEl = scrollTargetEl.find('a'),
