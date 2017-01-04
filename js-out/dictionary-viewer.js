8dcf4588be5925656ff05a4745c4bfd15ad4614f
btiernay <btiernay@oicr.on.ca>
2017-01-03 20:30:47 -0500

    Deployed b6fea59 with MkDocs version: 0.15.3

--- js-out/dictionary-viewer.js
+++ js-out/dictionary-viewer.js
@@ -95001,7 +95001,7 @@ webpackJsonp([0,2],[
 			"url": "https://github.com/benjamine/jsondiffpatch/issues"
 		},
 		"_id": "jsondiffpatch@0.1.43",
-		"_shasum": "d5966133c02b33f2d7335a35b323c96f9e85a6cd",
+		"_shasum": "6d257913157940b09c250d22583e182a1b8d0e29",
 		"_from": "git+https://github.com/cheapsteak/jsondiffpatch.git#1bf3df4875e4af1d17034649332ed19f33dbc4b5",
 		"_resolved": "git+https://github.com/cheapsteak/jsondiffpatch.git#1bf3df4875e4af1d17034649332ed19f33dbc4b5"
 	};
