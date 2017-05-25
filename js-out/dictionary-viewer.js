682fbfe6613ebde1af303fa5b23962d817a00230
SidSyrus <sidjoshi@hotmail.ca>
2017-05-25 14:12:29 -0500

    Deployed d2d0332 with MkDocs version: 0.15.3

--- js-out/dictionary-viewer.js
+++ js-out/dictionary-viewer.js
@@ -94972,7 +94972,7 @@ webpackJsonp([0,2],[
 			"/@icgc/dictionary-viewer"
 		],
 		"_resolved": "git+https://github.com/cheapsteak/jsondiffpatch.git#1bf3df4875e4af1d17034649332ed19f33dbc4b5",
-		"_shasum": "b0ef8694ac895794681780e4a07a0a98df2e0f7a",
+		"_shasum": "29d6d62fc37d435d3d205630c462457ee454390d",
 		"_shrinkwrap": null,
 		"_spec": "jsondiffpatch@git+https://github.com/cheapsteak/jsondiffpatch.git#1bf3df4875e4af1d17034649332ed19f33dbc4b5",
 		"_where": "/Users/sjoshi/dcc-docs/node_modules/@icgc/dictionary-viewer",
