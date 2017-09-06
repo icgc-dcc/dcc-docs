5128f9902455bc9951140316e27c9398422f56cb
Dušan Andrić <dusan.andric@oicr.on.ca>
2017-09-06 14:16:53 -0500

    Deployed d3a9d84 with MkDocs version: 0.16.3

--- js-out/dictionary-viewer.js
+++ js-out/dictionary-viewer.js
@@ -94948,7 +94948,7 @@ webpackJsonp([0,2],[
 						"directUrl": "https://raw.githubusercontent.com/cheapsteak/jsondiffpatch/1bf3df4875e4af1d17034649332ed19f33dbc4b5/package.json"
 					}
 				},
-				"/Users/hnahal/dcc-docs/node_modules/@icgc/dictionary-viewer"
+				"/Users/dandric/Documents/workspace/dcc-docs/node_modules/@icgc/dictionary-viewer"
 			]
 		],
 		"_from": "git+https://github.com/cheapsteak/jsondiffpatch.git#1bf3df4875e4af1d17034649332ed19f33dbc4b5",
@@ -94978,10 +94978,10 @@ webpackJsonp([0,2],[
 			"/@icgc/dictionary-viewer"
 		],
 		"_resolved": "git+https://github.com/cheapsteak/jsondiffpatch.git#1bf3df4875e4af1d17034649332ed19f33dbc4b5",
-		"_shasum": "78f05be3654a22102aa9501f8ae474141d80631a",
+		"_shasum": "fc49520eadd708d7e127d16ee0ef6b56f12bf6c6",
 		"_shrinkwrap": null,
 		"_spec": "jsondiffpatch@git+https://github.com/cheapsteak/jsondiffpatch.git#1bf3df4875e4af1d17034649332ed19f33dbc4b5",
-		"_where": "/Users/hnahal/dcc-docs/node_modules/@icgc/dictionary-viewer",
+		"_where": "/Users/dandric/Documents/workspace/dcc-docs/node_modules/@icgc/dictionary-viewer",
 		"author": {
 			"name": "Benjamin Eidelman",
 			"email": "beneidel@gmail.com"
